5065650039570c3d1856154d79ba5ee570946600
DongHyeok Lim <jh07050@gmail.com>
2023-05-19 19:57:39 +0900

    Feat: 임시 Main 구성 중 (기록)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 
-import logo from "../../assets/images/logo.png"
+import textLogo from "../../assets/images/textLogo.png"
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-in">
         <div className="left-tab">
-          <img className="logo-image" src={logo} alt="titleLogo"></img>
           <Link to="/">
-            <h4>CropSwipe</h4>
+            <img className="logo-image" src={textLogo} alt="titleLogo"></img>
           </Link>
         </div>
         <nav className="menu">
@@ -21,10 +20,10 @@ const Header = () => {
           <Link to="/transaction">
             <span>거래하기</span>
           </Link>
-          <Link to ="/signin">
+          <Link to="/signin">
             <span>로그인</span>
           </Link>
-          <Link to ="/signup">
+          <Link to="/signup">
             <span>회원가입</span>
           </Link>
         </nav>
---
+++ src/components/Header/MainHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Category from './Category'
+
+const MainHeader = () => {
+  return (
+    <div>
+      <Header />
+      <Category />
+    </div>
+  );
+}
+
+export default MainHeader
