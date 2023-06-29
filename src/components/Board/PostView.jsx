fe6799b9ade642f9fbf3e560bb5194ef75f93870
DongHyeok Lim <jh07050@gmail.com>
2023-06-29 20:22:15 +0900

    Feat: 게시글 상세 정보 및 댓글(조회, 등록, 삭제, 수정)

--- src/components/Board/PostView.jsx
+++ src/components/Board/PostView.jsx
@@ -1,49 +1,207 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { isLoggedInState } from "../../recoil/authState";
 import axios from "axios";
+
+import MainHeader from "../Header/MainHeader";
 import "./PostView.scss";
 
 const PostView = () => {
+  const navigate = useNavigate();
   const [postData, setPostData] = useState({});
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [editCommentId, setEditCommentId] = useState(null);
+  const [updatedCommentContent, setUpdatedCommentContent] = useState("");
   const { postId } = useParams();
+  const isLoggedIn = useRecoilValue(isLoggedInState);
 
+  // 해당 게시글 정보 가져오기
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/api/v1/post/posts/${postId}`)
       .then((response) => {
         setPostData(response.data);
-        console.log(response.data)
+        console.log(response.data);
       });
-  }, [postId]); // postId가 변경될 때마다 (각 해당하는 게시글을 들어갈 때마다)
+  }, [postId]);
+
+  // 해당 게시글의 댓글 가져오기
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/v1/post/posts/${postId}/comments`
+        );
+        setComments(response.data.comments);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getComments();
+  }, [postId]);
+
+  const handleWriteButton = () => {
+    if (!isLoggedIn) {
+      alert("글쓰기 권한이 없습니다.");
+      navigate("/signin");
+    } else {
+      navigate("/postform");
+    }
+  };
+
+  const isEditableComment = (commentId) => {
+    return editCommentId === commentId;
+  };
+
+  const handleCommentEdit = (commentId, content) => {
+    setEditCommentId(commentId);
+    setUpdatedCommentContent(content);
+  };
+
+  // 댓글 수정
+  const handleCommentUpdate = async (commentId, updatedContent) => {
+    try {
+      await axios.patch(
+        `${process.env.REACT_APP_API_URL}/api/v1/post/posts/${postId}/comments/${commentId}`,
+        { content: updatedContent }
+      );
+      alert("댓글이 수정되었습니다.");
+      setEditCommentId(null);
+      setUpdatedCommentContent("");
+      // 수정 후 최신 댓글 목록 가져오기
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/v1/post/posts/${postId}/comments`
+      );
+      setComments(response.data.comments);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 댓글 삭제
+  const handleCommentDelete = async (commentId) => {
+    try {
+      await axios.delete(
+        `${process.env.REACT_APP_API_URL}/api/v1/post/posts/${postId}/comments/${commentId}`
+      );
+      alert("댓글이 삭제되었습니다.");
+      // 삭제 후 최신 댓글 목록 가져오기
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/v1/post/posts/${postId}/comments`
+      );
+      setComments(response.data.comments);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 댓글 등록
+  const handleCommentRegister = async (event) => {
+    event.preventDefault();
+    if (!isLoggedIn) {
+      alert("댓글 권한이 없습니다.");
+      navigate("/signin");
+    } else {
+      try {
+        await axios.post(
+          `${process.env.REACT_APP_API_URL}/api/v1/post/posts/${postId}/comments`,
+          { content: comment }
+        );
+        alert("댓글이 등록되었습니다.");
+        setComment("");
+        // 등록 후 최신 댓글 목록 가져오기
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/v1/post/posts/${postId}/comments`
+        );
+        setComments(response.data.comments);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
-    <div>
-      <h2 align="center">게시글 상세정보</h2>
+    <>
+      <MainHeader />
+      <div className="qna-title">Q&A</div>
       {postData.post && (
-        <div className="voc-view-wrapper">
-          <div className="voc-view-row">
-            <label>게시글 번호</label>
-            <label>{postData.post.id}</label>
+        <div className="qna-post-wrapper">
+          <div className="qna-post-title">
+            <p>{postData.post.title}</p>
           </div>
-          <div className="voc-view-row">
-            <label>제목</label>
-            <label>{postData.post.title}</label>
+          <div className="qna-post-subtitle">
+            <p>{postData.post.author.nickname}</p>
+            <p className="qna-created_at">{postData.post.created_at}</p>
           </div>
-          <div className="voc-view-row">
-            <label>내용</label>
-            <div>{postData.post.content}</div>
-          </div>
-          <div className="voc-view-row">
-            <label>생성일</label>
-            <div>{postData.post.created_at}</div>
-          </div>
-          <div className="voc-view-row">
-            <label>수정일</label>
-            <div>{postData.post.updated_at}</div>
+          <div className="qna-post-view">
+            <p>{postData.post.content}</p>
           </div>
         </div>
       )}
-    </div>
+      <div className="qna-back">
+        <button className="qna-post-list">글 목록</button>
+        <button className="qna-post-write" onClick={handleWriteButton}>
+          글쓰기
+        </button>
+      </div>
+      <form className="comment-wrapper" method="POST">
+        <input
+          type="text"
+          placeholder="댓글을 입력해주세요."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button onClick={handleCommentRegister}>등록</button>
+      </form>
+      <div className="comment-view">
+        {comments.map((comment) => (
+          <div className="comment" key={comment.id}>
+            <div className="comment-wrapper">
+              <p className="comment-user">{comment.author.nickname}</p>
+              <p className="comment-created">{comment.created_at}</p>
+            </div>
+            <div>
+              {isEditableComment(comment.id) ? (
+                <input
+                  type="text"
+                  value={updatedCommentContent}
+                  onChange={(e) => setUpdatedCommentContent(e.target.value)}
+                />
+              ) : (
+                <p className="comment-detail">{comment.content}</p>
+              )}
+            </div>
+            <div>
+              {isLoggedIn && isEditableComment(comment.id) && (
+                <button
+                  onClick={() =>
+                    handleCommentUpdate(comment.id, updatedCommentContent)
+                  }
+                >
+                  수정 완료
+                </button>
+              )}
+              {isLoggedIn && !isEditableComment(comment.id) && (
+                <button
+                  onClick={() => handleCommentEdit(comment.id, comment.content)}
+                >
+                  수정
+                </button>
+              )}
+              {isLoggedIn && (
+                <button onClick={() => handleCommentDelete(comment.id)}>
+                  삭제
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
