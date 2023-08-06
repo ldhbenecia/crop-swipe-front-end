8fc96c09162b1253f05a2e6ae719e646ea4ff67e
DongHyeok Lim <jh07050@gmail.com>
2023-08-06 22:55:57 +0900

    Fix: KOE방지토큰 처리 해제
    해당 코드로 인해서 로그인 처리가 안되는 경우가 발생

--- src/components/Auth/SignIn/KakaoCallback.jsx
+++ src/components/Auth/SignIn/KakaoCallback.jsx
@@ -15,26 +15,30 @@ const KakaoCallback = () => {
     const handleCodeExchange = async () => {
       const code = new URL(window.location.href).searchParams.get("code");
       if (code) {
-        // 코드가 이미 처리되었는지 확인 -> KOE320 방지 토큰
-        const isCodeProcessed = localStorage.getItem("isCodeProcessed");
-        if (isCodeProcessed) {
-          navigate("/");
-          return;
-        }
 
-        localStorage.setItem("isCodeProcessed", "true");
+        // 사용 x -> 로그인 처리가 안되는 경우가 발생함
+        // // 코드가 이미 처리되었는지 확인 -> KOE320 방지 토큰
+        // const isCodeProcessed = localStorage.getItem("isCodeProcessed");
+        // if (isCodeProcessed) {
+        //   //navigate("/");
+        //   return;
+        // }
+
+        // localStorage.setItem("isCodeProcessed", "true");
 
         try {
           const response = await axios.get(
             `${process.env.REACT_APP_API_URL}/api/v1/user/login/kakao/callback?code=${code}`
           );
-          // console.log(response.data);
+           console.log(response.data);
 
           const accessToken = response.data.access;
           const refreshToken = response.data.refresh;
 
           setCookies("access-token", accessToken, { path: "/" });
           setCookies("refresh-token", refreshToken, { path: "/" });
+
+          console.log(response.data);
 
           setCookieState({ accessToken, refreshToken });
           setIsLoggedIn(true);
