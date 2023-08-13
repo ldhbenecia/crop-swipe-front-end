7cbfe0815e0c5d20224872124ee711dd6fde2bf9
DongHyeok Lim <jh07050@gmail.com>
2023-08-13 23:13:44 +0900

    Feat: 거래하기 페이지

---
+++ src/pages/Transaction/TransactionForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainHeader from '../../components/Header/MainHeader';
+import ProductForm from '../../components/Transaction/ProductForm';
+
+const TransactionForm = () => {
+  return (
+    <>
+      <MainHeader />
+      <ProductForm />
+    </>
+  );
+}
+
+export default TransactionForm
