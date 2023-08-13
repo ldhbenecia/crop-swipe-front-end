0de0fe121b40457eedd1e57d8be969414260d303
DongHyeok Lim <jh07050@gmail.com>
2023-08-13 23:12:34 +0900

    Design: BoardPage Footer 하단 고정

--- src/pages/BoardPage/BoardPage.jsx
+++ src/pages/BoardPage/BoardPage.jsx
@@ -3,15 +3,18 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import QnABoard from "../../components/Board/QnABoard";
+import "./BoardPage.scss"
 
 const BoardPage = () => {
   return (
-    <>
-    <Header />
-    <QnABoard />
-    <Footer />
-    </>
-  )
+    <div className="boardPage-wrapper">
+      <div className="boardPage-inner">
+        <Header />
+        <QnABoard />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default BoardPage;
